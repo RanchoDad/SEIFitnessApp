aa3396d205efd680a34e233341925ee94fe0cd0d
RanchoDad <joshuagarber215@gmail.com>
2023-04-24 19:50:39 -0700

    routes and controller ready for id's

--- routes/exercise.js
+++ routes/exercise.js
@@ -6,15 +6,15 @@ router.get('/', exerciseController.index);
 
 router.get('/new', exerciseController.new);
 
-// router.get('/:id', exerciseController.show);
+// router.get('/:id', exerciseController.show);   once there's data and id's, we can let this one out
 
 router.post('/', exerciseController.create);
 
-// router.delete('/:id', exerciseController.delete);
+// router.delete('/:id', exerciseController.delete);    same as show page
 
-// router.get('/:id/edit', exerciseController.edit);
+// router.get('/:id/edit', exerciseController.edit);    same as show page
 
-// router.put('/:id', exerciseController.update);
+// router.put('/:id', exerciseController.update);    same as show page
 
 //API Routes
 //router.post('/:id/api', exerciseController.createAPi)

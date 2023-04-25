63d3f5a382f2dee608180239c87d4c2a788018dd
bathant702 <bathant702nv@gmail.com>
2023-04-25 12:34:41 -0700

    add beginning of menu functions

---
+++ public/js/app.js
@@ -0,0 +1,28 @@
+// Overlay Controls
+const overlayMenuOpen = document.getElementById("overlay-menu");
+const overlayMenuClose = document.getElementById("closeOverlay");
+const overlayBioClose = document.getElementById("menu-link1");
+const overlayResumeClose = document.getElementById("menu-link2");
+const overlayResourceClose = document.getElementById("menu-link3");
+
+//Open Menu
+overlayMenuOpen.addEventListener("click", function openMenu(){
+  document.getElementById("myOverlay").style.width ="100%";
+});
+//Close Menu
+overlayMenuClose.addEventListener("click", function closeMenu(){
+  document.getElementById("myOverlay").style.width="0%";
+});
+//Close Bio
+overlayBioClose.addEventListener("click", function closeBioLink(){
+  document.getElementById("myOverlay").style.width="0%";
+});
+//Close Resume
+overlayResumeClose.addEventListener("click", function closeResumeLink(){
+  document.getElementById("myOverlay").style.width="0%";
+});
+//Close Resources
+overlayResourceClose.addEventListener("click", function closeResourceLink(){
+  document.getElementById("myOverlay").style.width="0%";
+});
+
